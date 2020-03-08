b1a385bbd12699d765adce4017444102222e80b2
shohag_shadaf <shohag.mbstu.it16030@gmail.com>
2020-03-08 09:26:39 +0600

    PartialThemeIntegrationAndDataTableIntegration

---
+++ PartialThemeIntegration&DataTableIntegration/MBSTU.LibrarySystem/MBSTU.LibrarySystem.Web/Areas/User/Controllers/FrontEndController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MBSTU.LibrarySystem.WEB.Areas.User.Controllers
+{
+    [Area("User")]
+    public class FrontEndController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
