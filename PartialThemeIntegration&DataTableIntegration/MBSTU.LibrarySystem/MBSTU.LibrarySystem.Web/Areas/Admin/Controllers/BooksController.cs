d6bc704f4fb3ea02632bb7daefbfb93fda6b3043
shohag_shadaf <shohag.mbstu.it16030@gmail.com>
2020-03-09 22:15:07 +0600

    assignment5 error

--- PartialThemeIntegration&DataTableIntegration/MBSTU.LibrarySystem/MBSTU.LibrarySystem.Web/Areas/Admin/Controllers/BooksController.cs
+++ PartialThemeIntegration&DataTableIntegration/MBSTU.LibrarySystem/MBSTU.LibrarySystem.Web/Areas/Admin/Controllers/BooksController.cs
@@ -4,17 +4,31 @@ using System.Linq;
 using System.Threading.Tasks;
 using MBSTU.LibrarySystem.WEB.Areas.Admin.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace MBSTU.LibrarySystem.WEB.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class BooksController : Controller
     {
+        private readonly IConfiguration _configuration;
+        public BooksController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         public IActionResult Index()
         {
-            var model = new BooksModel();
-         
+            var model = new BooksModel(_configuration);
             return View(model);
+        }
+
+        public IActionResult GetBooks()
+        {
+            var tableModel = new DataTablesAjaxRequestModel(Request);
+            var model = new BooksModel(_configuration);
+            var data = model.GetBooks(tableModel);
+            return Json(data);
         }
     }
 }
--- PartialThemeIntegration&DataTableIntegration/MBSTU.LibrarySystem/MBSTU.LibrarySystem.Web/Areas/Admin/Models/BooksModel.cs
+++ PartialThemeIntegration&DataTableIntegration/MBSTU.LibrarySystem/MBSTU.LibrarySystem.Web/Areas/Admin/Models/BooksModel.cs
@@ -1,11 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Linq;
 
 namespace MBSTU.LibrarySystem.WEB.Areas.Admin.Models
 {
-    public class BooksModel :AdminBaseModel
+    public class BooksModel : AdminBaseModel
     {
+        private readonly IBookService  _bookservice;
+        public BooksModel()
+        {
+            _bookservice = new BookService();
+        }
+
+        internal object GetBooks(DataTablesAjaxRequestModel tableModel)
+        {
+            var data = _bookService.GetBooks(
+                tableModel.PageIndex,
+                tableModel.PageSize,
+                tableModel.SearchText);
+
+            return new
+            {
+                recordsTotal = data.total,
+                recordsFiltered = data.totalDisplay,
+                data = (from record in data.records
+                        select new string[]
+                        {
+                                record.Title,
+                                record.Author,
+                                record.Edition,
+                                record.PublicationDate.ToString()
+                        }
+                    ).ToArray()
+
+            };
+        }
     }
+
+    
 }
