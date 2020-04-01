06f8d146425d044872b44c395b0177c77fedfe9b
shohag <shohag.mbstu.it16030@gmail.com>
2020-04-01 14:51:27 +0600

    AdminLteFrontEndPartialThemeIntegration

---
+++ AdminLteFrontEndPartialThemeIntegration/MBSTU.LibrarySystem/MBSTU.LibrarySystem.Web/Areas/Admin/Models/BooksModel.cs
@@ -0,0 +1,44 @@
+﻿using MBSTU.Library.Framwork;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MBSTU.LibrarySystem.Web.Areas.Admin.Models
+{
+        public class BooksModel : AdminBaseModel
+        {
+            private readonly IBookService _bookService;
+            public BooksModel(IConfiguration configuration)
+            {
+                _bookService = new BookService(configuration.GetConnectionString("DefaultConnection"));
+            }
+
+            internal object GetBooks(DataTablesAjaxRequestModel tableModel)
+            {
+                var data = _bookService.GetBooks(
+                    tableModel.PageIndex,
+                    tableModel.PageSize,
+                    tableModel.SearchText,
+                    tableModel.GetSortText(new string[] { "Title", "Author", "Edition", "PublicationDate" }));
+
+                return new
+                {
+                    recordsTotal = data.total,
+                    recordsFiltered = data.totalDisplay,
+                    data = (from record in data.records
+                            select new string[]
+                            {
+                                record.Title,
+                                record.Author,
+                                record.Edition,
+                                record.PublicationDate.ToString()
+                            }
+                        ).ToArray()
+
+                };
+            }
+        }
+    }
+
---
+++ AdminLteFrontEndPartialThemeIntegration/MBSTU.LibrarySystem/MBSTU.LibrarySystem.Web/Areas/Admin/Models/DashboardModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MBSTU.LibrarySystem.Web.Areas.Admin.Models
+{
+    public class DashboardModel :AdminBaseModel
+    {
+      
+    }
+}
