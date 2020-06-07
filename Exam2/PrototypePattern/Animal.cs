df0acf55db0cf2f0dcf525a2fe45ba9b56a55947
shohag_shadaf <shohag.mbstu.it16030@gmail.com>
2020-06-06 22:24:02 -0700

    updated

---
+++ Exam2/PrototypePattern/Animal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrototypePattern
+{
+    public class Animal : ICloneable
+    {
+        public string Name { get; set; }
+        public string Colour { get; set; }
+
+        public Animal Clone()
+        {
+            return new Animal()
+            {
+                Name = this.Name,
+                Colour = this.Colour
+            };
+        }
+
+        object ICloneable.Clone()
+        {
+            return new Animal()
+            {
+                Name = this.Name,
+                Colour = this.Colour
+            };
+        }
+    }
+}
