500bdc8e823123b421fc11dca2702dc7bd82f0d2
shohag_shadaf <shohag.mbstu.it16030@gmail.com>
2020-06-06 21:39:38 -0700

    updated

--- Exam2/Exam2/ques2.cs
+++ Exam2/Exam2/ques2.cs
@@ -1,47 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Text;
-using System.Xml.Schema;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Globalization;
+//using System.Text;
+//using System.Xml.Schema;
 
-namespace Exam2
-{
-    public class _2ques
-    {
-        public static void Main()
-        {
-            var n = int.Parse(Console.ReadLine());
-            int temp = n - 1;
-            int i,j;
-            for ( i = 0; i < n; i++)
-            {
-                for( j = 0; j < temp; j++)
-                {
-                    Console.Write(" ");
-                }
-                for (j = 0; j <= i; j++)
-                {
-                    Console.Write("* ");
-                }
-                Console.WriteLine();
-                temp--;
-            }
-             temp = 0;
-            for (i = n; i > 0; i--)
-            {
-                for(j = 0; j < temp; j++)
-                {
-                    Console.Write(" ");
-                }
-                for (j = 0; j < i; j++)
-                {
-                    Console.Write("* ");
-                }
-                Console.WriteLine();
-                temp++;
-            }
+//namespace Exam2
+//{
+//    public class _2ques
+//    {
+//        public static void Main()
+//        {
+//            var n = int.Parse(Console.ReadLine());
+//            int temp = n - 1;
+//            int i,j;
+//            for ( i = 0; i < n; i++)
+//            {
+//                for( j = 0; j < temp; j++)
+//                {
+//                    Console.Write(" ");
+//                }
+//                for (j = 0; j <= i; j++)
+//                {
+//                    Console.Write("* ");
+//                }
+//                Console.WriteLine();
+//                temp--;
+//            }
+//             temp = 0;
+//            for (i = n; i > 0; i--)
+//            {
+//                for(j = 0; j < temp; j++)
+//                {
+//                    Console.Write(" ");
+//                }
+//                for (j = 0; j < i; j++)
+//                {
+//                    Console.Write("* ");
+//                }
+//                Console.WriteLine();
+//                temp++;
+//            }
 
             
-        }
-    }
-}
+//        }
+//    }
+//}
