f7f2fdbdeb053dd2359978b6ca96b0da2c51e5d8
shohag_shadaf <shohag.mbstu.it16030@gmail.com>
2020-06-06 21:01:28 -0700

    exam

--- Exam2/Exam2/ques1.cs
+++ Exam2/Exam2/ques1.cs
@@ -1,32 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
 
-namespace Exam2
-{
-    public class ques1
-    {
-        static void Main(string[] args)
-        {
-            string name; int age; string city;
-            var toupleList = new List<(int,string, string)>();
-            var n = int.Parse(Console.ReadLine());
+//namespace Exam2
+//{
+//    public class ques1
+//    {
+//        static void Main(string[] args)
+//        {
+//            string name; int age; string city;
+//            var toupleList = new List<(int,string, string)>();
+//            var n = int.Parse(Console.ReadLine());
 
-            for (int i = 0; i <n; i++)
-            {
-                var line = Console.ReadLine().Split();
-                name = line[0];
-                age = int.Parse(line[1]);
-                city = line[2];
-                toupleList.Add((age,name,city));
-            }
-            toupleList.Sort();
-            foreach (var item in toupleList)
-            {
-                Console.WriteLine($"{item.Item1} { item.Item2} { item.Item3}");
-            }
+//            for (int i = 0; i <n; i++)
+//            {
+//                var line = Console.ReadLine().Split();
+//                name = line[0];
+//                age = int.Parse(line[1]);
+//                city = line[2];
+//                toupleList.Add((age,name,city));
+//            }
+//            toupleList.Sort();
+//            foreach (var item in toupleList)
+//            {
+//                Console.WriteLine($"{item.Item1} { item.Item2} { item.Item3}");
+//            }
         
 
-        }
-    }
-}
+//        }
+//    }
+//}
--- Exam2/Exam2/ques2.cs
+++ Exam2/Exam2/ques2.cs
@@ -1,10 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Text;
+using System.Xml.Schema;
 
 namespace Exam2
 {
-    class _2ques
+    public class _2ques
     {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            int temp = n - 1;
+            int i,j;
+            for ( i = 0; i < n; i++)
+            {
+                for( j = 0; j < temp; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (j = 0; j <= i; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+                temp--;
+            }
+             temp = 0;
+            for (i = n; i > 0; i--)
+            {
+                for(j = 0; j < temp; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (j = 0; j < i; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+                temp++;
+            }
+
+            
+        }
     }
 }
