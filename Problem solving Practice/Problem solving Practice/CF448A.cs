1af241356e0672794108ec7a2df6e28ba2d107c9
shohag_shadaf <shohag.mbstu.it16030@gmail.com>
2020-06-26 13:13:50 -0700

    updated

--- Problem solving Practice/Problem solving Practice/CF448A.cs
+++ Problem solving Practice/Problem solving Practice/CF448A.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using System.IO.Pipes;
 
 namespace Problem_solving_Practice
 {
-    class Program
+    class CF448A
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var cup = Console.ReadLine().Split(' ');
+            var madel = Console.ReadLine().Split(' ');
+            var n = int.Parse(Console.ReadLine());
+            int csum = 0, msum = 0;
+            for(int i = 0; i < 3; i++)
+            {
+                 csum += int.Parse(cup[i]);
+                 msum += int.Parse(madel[i]);
+            }
+            int ans = csum / 5;
+            if (csum % 5 != 0)
+                ans++;
+            ans += msum / 10;
+            if (msum % 10 != 0)
+                ans++;
+            if(ans <=n)
+                Console.WriteLine("YES");
+            else
+                Console.WriteLine("NO");
+            
+            
         }
     }
 }
