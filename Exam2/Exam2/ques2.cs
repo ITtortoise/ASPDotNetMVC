7e750d0bf63fc1fd3ccb894019bb2593eab0c855
shohag_shadaf <shohag.mbstu.it16030@gmail.com>
2020-06-26 11:15:54 -0700

    updated

--- Exam2/Exam2/ques2.cs
+++ Exam2/Exam2/ques2.cs
@@ -1,17 +1,52 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Globalization;
-//using System.Text;
-//using System.Xml.Schema;
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Xml.Schema;
 
-//namespace Exam2
-//{
-//    public class _2ques
-//    {
-//        public static void Main()
-//        {
-            
+namespace Exam2
+{
+    public class _2ques
+    {
+        public static void Main()
+        {
+            //INPUT FORMAT FOR 24 TO 12 CONVERSION 00:00 (no space)
+            //INPUT FORMAT FOR 12 TO 24 CONVERSION 00:00AM OR 00:00PM (no space)
+            Console.WriteLine("Press 1 for convert time 24 hour to 12 hour OR 2 for convert time 12 hour to 24 hour :");
+            int n = int.Parse(Console.ReadLine());
+            switch (n)
+            {
+                case 1:
+                    {
+                        var line = Console.ReadLine().Split(':');
+                        int h = int.Parse(line[0]);
+                        int m = int.Parse(line[0]);
+                        if (h <= 12)
+                            Console.WriteLine($"{h}:{m} AM");
+                        else
+                            Console.WriteLine($"{h - 12}:{m} PM");
+                        
+                        break;
+                    }
+                case 2:
+                    {
+                        var line = Console.ReadLine();
+                        var h = "";var m = "";var t = "";
+                        h += line[0];h += line[1];
+                        m += line[3];m += line[4];
+                        t += line[5];t += line[6];
+                        int hour = int.Parse(h);
+                        int minute = int.Parse(m);
+                        if(t=="AM")
+                            Console.WriteLine($"{h}:{m}");
+                        else 
+                        {
+                            Console.WriteLine($"{hour+12}:{m}");
+                        }
+                        break;
+                    }
 
-//        }
-//    }
-//}
+            }
+        }
+    }
+}
--- Exam2/Exam2/ques3.cs
+++ Exam2/Exam2/ques3.cs
@@ -1,45 +1,45 @@
-﻿using System;
-using System.Collections.Concurrent;
-using System.Collections.Generic;
-using System.Text;
+﻿//using System;
+//using System.Collections.Concurrent;
+//using System.Collections.Generic;
+//using System.Text;
 
-namespace Exam2
-{
-    public class ques3
-    {
-        static void Main()
-        {
-            var n = int.Parse(Console.ReadLine());
-            int temp = n - 1;
-            int i, j;
-            for (i = 0; i < n; i++)
-            {
-                for (j = 0; j < temp; j++)
-                {
-                    Console.Write(" ");
-                }
-                for (j = 0; j <= i; j++)
-                {
-                    Console.Write("* ");
-                }
-                Console.WriteLine();
-                temp--;
-            }
-            temp = 0;
-            for (i = n; i > 0; i--)
-            {
-                for (j = 0; j < temp; j++)
-                {
-                    Console.Write(" ");
-                }
-                for (j = 0; j < i; j++)
-                {
-                    Console.Write("* ");
-                }
-                Console.WriteLine();
-                temp++;
-            }
+//namespace Exam2
+//{
+//    public class ques3
+//    {
+//        static void Main()
+//        {
+//            var n = int.Parse(Console.ReadLine());
+//            int temp = n - 1;
+//            int i, j;
+//            for (i = 0; i < n; i++)
+//            {
+//                for (j = 0; j < temp; j++)
+//                {
+//                    Console.Write(" ");
+//                }
+//                for (j = 0; j <= i; j++)
+//                {
+//                    Console.Write("* ");
+//                }
+//                Console.WriteLine();
+//                temp--;
+//            }
+//            temp = 0;
+//            for (i = n; i > 0; i--)
+//            {
+//                for (j = 0; j < temp; j++)
+//                {
+//                    Console.Write(" ");
+//                }
+//                for (j = 0; j < i; j++)
+//                {
+//                    Console.Write("* ");
+//                }
+//                Console.WriteLine();
+//                temp++;
+//            }
 
-        }
-    }
-}
+//        }
+//    }
+//}
