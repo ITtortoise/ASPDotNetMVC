12b904e7a5604ab9b95c359c196e1768454be839
shohag_shadaf <shohag.mbstu.it16030@gmail.com>
2020-06-27 09:15:54 -0700

    CsharpFeaturesAndDesignPatternAdded

---
+++ DesignPattern/DesignPattern/IceCream.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuilderPattern
+{
+    public class IceCream
+    {
+        public string Flavour { get; set; }
+        public bool HasCherry { get; set; }
+        public bool HasChocolet { get; set; }
+        public bool HasNuts { get; set; }
+    }
+}
