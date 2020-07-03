03c9d40cfca8037d36b566711ae0d442cc6ba7f0
shohag_shadaf <shohag.mbstu.it16030@gmail.com>
2020-07-03 00:15:54 -0700

    dailyexpense ass8 added

---
+++ AuthenticationDemo/AuthenticationDemo/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Membership.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthenticationDemo.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles ="administrator,superuser")]
+    public class DashboardController : Controller
+    {
+        private readonly RoleManager _roleManager;
+        public DashboardController(RoleManager roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        public IActionResult Index()
+        {
+            // Role based, Policy Based, Claim based
+
+            // Seed
+            return View();
+        }
+
+        [AllowAnonymous]
+        public async Task<IActionResult> CreateRole()
+        {
+            await _roleManager.CreateAsync(new Membership.Entities.Role("administrator"));
+            await _roleManager.CreateAsync(new Membership.Entities.Role("superuser"));
+            return View();
+        }
+    }
+}
